3f7db92360894863d4314ce8f2e91a1b3bc0137d
Mickelmore1 <190520509+Mickelmore1@users.noreply.github.com>
2025-03-14 23:15:56 +0000

    Update

---
+++ script.js
@@ -0,0 +1,62 @@
+const search = document.getElementById("search");
+const address = document.getElementById("location");
+const temperature = document.getElementById("temperature");
+const conditions = document.getElementById("current-condition");
+const highTemp = document.getElementById("high");
+const lowTemp = document.getElementById("low");
+
+search.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    weather(search.value);
+    search.value = "";
+  }
+});
+
+async function weather(query) {
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${query}?unitGroup=uk&key=SLQ4AYDX9QUCP7SBDSFMGA47Q&contentType=json`,
+      {
+        method: "GET",
+        headers: {},
+      }
+    );
+    const data = await response.json();
+    ui.updateDisplay(data);
+    ui.addressLineBreak();
+    ui.updateHour(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function uiControls() {
+  return {
+    updateDisplay(data) {
+      address.innerText = data.resolvedAddress;
+      temperature.innerText = data.currentConditions.temp + "°";
+      conditions.innerText = data.currentConditions.conditions;
+      highTemp.innerText = "H: " + data.days[0].tempmax + "°";
+      lowTemp.innerText = "L: " + data.days[0].tempmin + "°";
+    },
+    addressLineBreak() {
+      const string = address.innerText;
+      address.innerText = string.replaceAll(", ", "\n");
+    },
+    formatHourDisplay(data) {
+      const time = data.currentConditions.datetime;
+      const hour = Number(time.slice(0, 2));
+      return hour;
+    },
+    updateHour(data) {
+      const hours = document.querySelectorAll(".hour");
+      console.log(hours);
+      for (i = 0; i < 24; i++) {
+        hours[i].children[1].src = "img/" + data.days[0].hours[i].icon + ".png";
+        hours[i].children[2].innerText = data.days[0].hours[i].temp + "°";
+      }
+    },
+  };
+}
+
+const ui = uiControls();
